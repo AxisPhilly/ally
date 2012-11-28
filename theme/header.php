0c7a0202c387daf96ed2cb0d9d3b2c59df985436
newsapps <newsapps@newsappss-iMac.local>
2012-11-28 13:04:31 -0500

    initial template commit

---
+++ theme/header.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>AxisPhilly</title>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0, user-scalable=0">
+  <link href='http://fonts.googleapis.com/css?family=Open+Sans:400italic,400,800,600' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo( 'stylesheet_url' ); ?>" />
+</head>
+<body>
+  <!-- Fixed header -->
+  <div class="fixed contain-to-grid">
+    <nav class="top-bar">
+      <ul>
+        <li class="name">
+          <h1><a href="/demo/">AxisPhilly</a></h1>
+        </li>
+        <li class="toggle-topbar has-button">
+          <span class="tiny secondary button menu-button">
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
+          </span>
+        </li>
+      </ul>
+      <section>
+        <ul class="left">
+          <li class="divider"></li>
+          <li class="has-dropdown">
+            <a href="#">Our Projects</a>
+            <ul class="dropdown">
+              <li><a href="/demo/project/avi">AVI</a></li>
+              <li><a href="#">Lobbying</a></li>
+            </ul>
+          </li>
+          <li class="divider"></li>
+          <li><a href="#">Tools &amp; Data</a></li>
+          <li class="divider"></li>
+          <li><a href="/demo/about">About</a></li>
+          <li class="search">
+          <li class="divider"></li>
+          <li>
+            <span class="social mobile">
+              <a href="twitter"><i class="social-foundicon-twitter"></i></a>
+              <a href="facebook"><i class="social-foundicon-facebook"></i></a>
+              <a href="googleplus"><i class="social-foundicon-google-plus"></i></a>
+              <a href="rss"><i class="social-foundicon-rss"></i></a>
+            </span>
+            <ul class="social desktop">
+              <li><a href="twitter"><i class="social-foundicon-twitter"></i></a></li>
+              <li><a href="facebook"><i class="social-foundicon-facebook"></i></a></li>
+              <li><a href="googleplus"><i class="social-foundicon-google-plus"></i></a></li>
+              <li><a href="rss"><i class="social-foundicon-rss"></i></a></li>
+            </ul>
+          </li>
+        </ul>
+        <ul class="right">
+          <li class="search">
+            <form class="collapse">
+              <input type="search" placeholder="Search"></input>
+            </form>
+          </li>
+        </ul>
+      </section>
+    </nav>
+  </div><!-- End fixed header -->
