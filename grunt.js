b0b6739e508fa1559a9e37421e04bf715355d20f
Casey Thomas <cpthomas@gmail.com>
2012-12-05 12:16:11 -0500

    Update dist directories

--- grunt.js
+++ grunt.js
@@ -43,11 +43,11 @@ module.exports = function(grunt) {
           'javascripts/foundation/jquery.foundation.placeholder.js',
           'javascripts/foundation/app.js'
         ],
-        dest: 'theme/javascripts/foundation.js'
+        dest: 'javascripts/foundation.js'
       },
       libraries: {
         src: 'javascripts/contrib/*',
-        dest: 'theme/javascripts/libraries.<%= pkg.version %>.min.js'
+        dest: 'javascripts/libraries.<%= pkg.version %>.min.js'
       },
       sass: {
         src: [
@@ -56,24 +56,24 @@ module.exports = function(grunt) {
           'stylesheets/function-overrides.css',
           'stylesheets/compass.css'
         ],
-        dest: 'theme/style.css'
+        dest: 'style.css'
       }
     },
     min: {
       site: {
         src: 'javascripts/site.js',
-        dest: 'theme/javascripts/site.<%= pkg.version %>.min.js'
+        dest: 'javascripts/site.<%= pkg.version %>.min.js'
       },
       foundation: {
-        src: 'theme/javascripts/foundation.js',
-        dest: 'theme/javascripts/foundation.min.js'
+        src: 'javascripts/foundation.js',
+        dest: 'javascripts/foundation.min.js'
       }
     }
     /*compass: {
       dist: {
         options: {
           sassDir: 'sass',
-          cssDir: 'theme'
+          cssDir: ''
         }
       }
     }*/
