f376320f276aac86a3964032e7a8a392eb0215b3
jfrankl <jfinbox@comcast.net>
2012-12-05 10:36:15 -0500

    load stories, distinguish between original reporting and external sources

--- theme/index.php
+++ theme/index.php
@@ -1,6 +1,5 @@
 <!-- Loads header.php -->
 <?php get_header(); ?>
-
 <!-- Content -->
   <div class="content-container">
     <div class="row">
@@ -9,35 +8,19 @@
         <li class="current"><a href="#">Actual Value Initiative</a></li>
       </ul>
     </div>
-
     <!-- News Container -->
     <div id="news-container">
-      
       <div class="row">
         <header class="project-header twelve columns">
           <h1>Actual Value Initiative</h1>
         </header>
       </div>
-
       <!-- Header Row -->
       <div class="row">
         <!-- Background -->
         <div id="background" class="five columns">
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco.</p>
-
-          <p>Laboris nisi ut aliquip ex ea commodo
-          consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
-          cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-          proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Fored dfdm lorem
-          impsum dolor.</p>
-
-          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-          tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-          quis nostrud exercitation ullamco.</p>
+ <?php echo category_description( get_category_by_slug('avi')->term_id ); ?>
         </div>
-
         <div id="background-compressed">
           <ul class="accordion">
             <li>
@@ -51,16 +34,34 @@
             </li>
           </ul>
         </div>
-
         <!-- Features -->
         <div id="feature-container" class="seven columns">
           <div id="featured">
             <!-- features go here -->
+<?php
+  query_posts( array( 'posts_per_page' => 1, 'post_status' => 'publish' , 'category_name' => 'featured' , 'post_type' => array( 'post' , 'external_post', 'external_tool', 'wp_tool') ) );
+  
+  if (have_posts()) :
+  while (have_posts()) :
+  the_post();
+  ?>
+    <?php if (has_post_thumbnail( $post->ID ) ): ?>  
+      <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+      <img src="<?php echo $image[0]; ?>">
+    <?php endif; ?>
+    <div class="caption">
+      <h4 class="headline"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+      <span class="byline">by <?php the_author(); ?>, <?php the_time(); ?></span>
+      <p><?php the_excerpt(); ?></p>
+    </div>
+<?php
+  //loop content
+  endwhile;
+  endif;
+  ?>
           </div>
         </div><!-- End Features -->
-
       </div><!-- End Header Row -->
-
       <div class="project-section-nav small" data-spy="affix" data-offset-top="360">
         <div class="row">
           <dl class="tabs three-up">
@@ -70,7 +71,6 @@
           </dl>
         </div>
       </div>
-
       <div class="project-section-nav large" data-spy="affix" data-offset-top="570">
         <div class="row">
           <dl class="tabs three-up">
@@ -80,13 +80,47 @@
           </dl>
         </div>
       </div>
-
       <ul class="tabs-content">
         <li class="active" id="feedTab">
           <a name="stories"></a>
           <div class="row">
             <div id="stories" class="twelve columns">
-              <div class="items"></div>
+              <div class="items">
+                <?php
+  query_posts( array( 'post_status' => 'publish' , 'category_name' => 'avi' , 'post_type' => array( 'post' , 'external_post') ) );
+  if (have_posts()) :
+  while (have_posts()) :
+  the_post();
+  ?>
+    <div class="eight mobile-two columns">
+      <h4><a name="<?php the_id(); ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+      <div class="byline hide-for-small">
+
+<!-- Check to see if this is an external_post. If so, display Source and Source URL instead of Author. -->
+
+      <?php
+
+
+
+
+
+      if (get_post_type() == 'external_post') echo "Source: <a href='" . get_post_meta( $post->ID, '_url_name', true)."'>".get_post_meta( $post->ID, '_source_name', true) . "</a>"; if ((get_post_type() != 'external_post' )) echo "by " . get_the_author(); ?>
+      </div>
+      <div class="datetime hide-for-small"><?php the_time(); ?> </div>
+      <p class="hide-for-small"><?php the_excerpt(); ?></p>
+    </div>
+    <div class="four mobile-two columns">
+    <?php if (has_post_thumbnail( $post->ID ) ): ?>  
+      <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+      <img src="<?php echo $image[0]; ?>">
+    <?php endif; ?>
+    </div>
+<?php
+  //loop content
+  endwhile;
+  endif;
+  ?>
+              </div>
             </div>
           </div>
         </li>
@@ -94,7 +128,19 @@
           <a name="tools-and-data"></a>
           <div class="row">
             <div id="tools-and-data" class="twelve columns">
-              <div class="items"></div>
+              <div class="items">
+                <?php
+  query_posts( array( 'post_status' => 'publish' , 'category_name' => 'avi' , 'post_type' => array( 'wp_tool' , 'external_tool') ) );
+  
+  if (have_posts()) :
+  while (have_posts()) :
+  the_post();
+  the_title();
+  //loop content
+  endwhile;
+  endif;
+  ?>
+              </div>
             </div>
           </div>
         </li>
@@ -108,42 +154,36 @@
               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
               proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
               consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
               cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
               proident, sunt in culpa qui officia deserunt mollit anim id est laborum
-
               Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
               tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
               quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
@@ -154,20 +194,14 @@
           </div>
         </li>
       </ul>
-
     </div><!-- End News Container -->
-
     <footer></footer>
-
   </div><!-- End Content Container -->
-
  <!-- Included JS Files (Compressed) -->
-
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.3/jquery.min.js" type="text/javascript"></script>
   <script src="<?php bloginfo( 'template_directory' ); ?>/javascripts/libraries.0.0.1.min.js" type="text/javascript"></script>
   <script src="<?php bloginfo( 'template_directory' ); ?>/javascripts/site.0.0.1.min.js" type="text/javascript"></script>
   <script src="<?php bloginfo( 'template_directory' ); ?>/javascripts/foundation.min.js" type="text/javascript"></script>
-
   <!-- Templates -->
   <!-- Feature story -->
   <script type="text/template" id="feature-story-carousel-template">
@@ -220,7 +254,6 @@
   <!-- Single article sidebar template-->
   <script type="text/template" id="single-article-sidebar-template">
     <div class="moving-container" data-spy="affix" data-offset-top="70">
-      
       <div class="social-container">
         <strong>Share this article</strong>
         <div class="shorturl-container">
@@ -237,17 +270,14 @@
                   target="_blank"><i class="general-foundicon-mail"></i></a>
         </div>
       </div>
-      
       <div class="project-info-container">
         <p>
           <strong>This article is part of our series on <a href="#">AVI</a>.
           Read more from the series:</strong>
         </p>
         <div class="recent-stories">
-
         </div>
       </div>
-
     </div>
   </script>
   <!-- Recent story template -->
@@ -277,7 +307,6 @@
       <%= fullText %>
     </div>
     <nav class="article-navigation row">
-
     </nav>
   </script>
   <!-- Story Navigation item -->  
@@ -287,6 +316,5 @@
       <span><%= headline %></span>
     </a>
   </script>
-
 <!-- Loads footer.php -->
 <?php get_footer(); ?>
