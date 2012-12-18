1c91739b21ce65f5c492bbd3e9bc2b458a2a7ee8
Casey Thomas <cpthomas@gmail.com>
2012-12-18 12:23:15 -0500

    Fix indentations. Cleanup code

--- functions.php
+++ functions.php
@@ -1,8 +1,6 @@
 <?php
 
-
 function dimox_breadcrumbs() {
- 
   $showOnHome = 0; // 1 - show breadcrumbs on the homepage, 0 - don't show
   $delimiter = '&raquo;'; // delimiter between crumbs
   $home = 'Home'; // text for the 'Home' link
@@ -109,185 +107,167 @@ function dimox_breadcrumbs() {
 
 
 // Create source_meta Custom Field
+add_action('add_meta_boxes', 'source_meta');
 
-add_action( 'add_meta_boxes', 'source_meta' );
-        function source_meta() {
-                add_meta_box( 'source_meta', 'Source', 'source_callback', 'external_post', 'normal', 'high' );
-                }
+function source_meta() {
+  add_meta_box('source_meta', 'Source', 'source_callback', 'external_post', 'normal', 'high');
+}
 
-        function source_callback( $post ) {
-            $source_name = get_post_meta( $post->ID, '_source_name', true);
-            echo 'What is the name of the publication that created this story? <em>ex: Philadelphia City Paper</em><br><input type="text" name="source_name" style="width: 100%" value="'.$source_name.'"/>';
-            }
+function source_callback( $post ) {
+  $source_name = get_post_meta($post->ID, '_source_name', true);
+  echo 'What is the name of the publication that created this story? <em>ex: Philadelphia City Paper</em><br><input type="text" name="source_name" style="width: 100%" value="'.$source_name.'"/>';
+}
 
 // Save contents of source_meta Custom Field
-
 add_action( 'save_post', 'source_save' );
-        function source_save( $post_ID ) {
-            global $post;
-           
-            if (isset( $_POST ) ) {
-                update_post_meta( $post_ID, '_source_name', strip_tags( $_POST['source_name'] ) );
-            
-        }
-        }      
 
+function source_save($post_ID) {
+  global $post;
+           
+  if (isset($_POST)) {
+    update_post_meta($post_ID, '_source_name', strip_tags($_POST['source_name']));
+  }
+}
 
 // Create url_meta Custom Field
+add_action('add_meta_boxes', 'url_meta');
 
-add_action( 'add_meta_boxes', 'url_meta' );
-        function url_meta() {
-                add_meta_box( 'url_meta', 'URL', 'url_callback', 'external_post', 'normal', 'high' );
-                add_meta_box( 'url_meta', 'URL', 'url_callback', 'external_tool', 'normal', 'high' );
-                }
+function url_meta() {
+  add_meta_box('url_meta', 'URL', 'url_callback', 'external_post', 'normal', 'high');
+  add_meta_box('url_meta', 'URL', 'url_callback', 'external_tool', 'normal', 'high');
+}
 
-        function url_callback( $post ) {
-            $url_name = get_post_meta( $post->ID, '_url_name', true);
-            echo 'What is the URL for the story? <em>http://citypaper.net/storyname/storyname/</em><br><input type="text" name="url_name" style="width: 100%" value="'.$url_name.'"/>';
-            }
+function url_callback($post) {
+  $url_name = get_post_meta($post->ID, '_url_name', true);
+  echo 'What is the URL for the story? <em>http://citypaper.net/storyname/storyname/</em><br><input type="text" name="url_name" style="width: 100%" value="'.$url_name.'"/>';
+}
 
 // Save contents of url_meta Custom Field
+add_action('save_post', 'url_save');
 
-add_action( 'save_post', 'url_save' );
-        function url_save( $post_ID ) {
-            global $post;
+function url_save($post_ID) {
+  global $post;
            
-            if (isset( $_POST ) ) {
-                update_post_meta( $post_ID, '_url_name', strip_tags( $_POST['url_name'] ) );
-            
-        }
-        }      
-
+  if (isset($_POST)) {
+    update_post_meta( $post_ID, '_url_name', strip_tags( $_POST['url_name'] ) );
+  }
+}      
 
 // Enables Featured Images
-
-add_theme_support( 'post-thumbnails' ); 
+add_theme_support('post-thumbnails'); 
 
 // http://wordpress.stackexchange.com/questions/15376/how-to-set-default-screen-options
-
 // add_action('user_register', 'set_user_metaboxes');
 add_action('admin_init', 'set_user_metaboxes');
+
 function set_user_metaboxes($user_id=NULL) {
+  // These are the metakeys we will need to update
+  $meta_key['order'] = 'meta-box-order_post';
+  $meta_key['hidden'] = 'metaboxhidden_post';
 
-    // These are the metakeys we will need to update
-    $meta_key['order'] = 'meta-box-order_post';
-    $meta_key['hidden'] = 'metaboxhidden_post';
+  // So this can be used without hooking into user_register
+  if (!$user_id) {
+    $user_id = get_current_user_id();
+  }
 
-    // So this can be used without hooking into user_register
-    if ( ! $user_id)
-        $user_id = get_current_user_id(); 
+  // Set the default order if it has not been set yet
+  if (!get_user_meta( $user_id, $meta_key['order'], true)) {
+    $meta_value = array(
+      'side' => 'submitdiv,formatdiv,categorydiv,tagsdiv-post_tag,postimagediv',
+      'normal' => 'postexcerpt,source_meta,url_meta,postcustom,commentstatusdiv,commentsdiv,trackbacksdiv,slugdiv,authordiv,revisionsdiv',
+      'advanced' => '',
+    );
+    
+    update_user_meta( $user_id, $meta_key['order'], $meta_value );
+  }
 
-    // Set the default order if it has not been set yet
-    if ( ! get_user_meta( $user_id, $meta_key['order'], true) ) {
-        $meta_value = array(
-            'side' => 'submitdiv,formatdiv,categorydiv,tagsdiv-post_tag,postimagediv',
-            'normal' => 'postexcerpt,source_meta,url_meta,postcustom,commentstatusdiv,commentsdiv,trackbacksdiv,slugdiv,authordiv,revisionsdiv',
-            'advanced' => '',
-        );
-        update_user_meta( $user_id, $meta_key['order'], $meta_value );
-    }
-
-    // Set the default hiddens if it has not been set yet
-    if ( ! get_user_meta( $user_id, $meta_key['hidden'], true) ) {
-        $meta_value = array('postcustom','trackbacksdiv','commentstatusdiv','commentsdiv','slugdiv','revisionsdiv');
-        update_user_meta( $user_id, $meta_key['hidden'], $meta_value );
-    }
+  // Set the default hiddens if it has not been set yet
+  if (!get_user_meta( $user_id, $meta_key['hidden'], true)) {
+    $meta_value = array('postcustom','trackbacksdiv','commentstatusdiv','commentsdiv','slugdiv','revisionsdiv');
+    update_user_meta( $user_id, $meta_key['hidden'], $meta_value );
+  }
 }
 
 // http://wp.tutsplus.com/tutorials/theme-development/innovative-uses-of-wordpress-post-types-and-taxonomies/
 // Creates "External Post" Post Type
 
 function create_post_type() {  
+  // register external_post as a Custom Post Type
+  register_post_type( 'external_post',  
+    array(  
+      'labels' => array(  
+        'name' => __('External Posts'),  
+        'singular_name' => __('External Post')  
+      ),
+      'public' => true,
+      'menu_position' => 5,
+      'supports' => array('title', 'excerpt'),
+      'rewrite' => array('slug' => 'external','with_front' => false)  
+    )  
+  );  
 
-// register external_post as a Custom Post Type
+  // connect external_post to category taxonomy
+  register_taxonomy_for_object_type('category', 'external_post');
 
-    register_post_type( 'external_post',  
-        array(  
-            'labels' => array(  
-                'name' => __( 'External Posts' ),  
-                'singular_name' => __( 'External Post' )  
-            ),  
-        'public' => true,  
-        'menu_position' => 5,  
-        'supports' => array( 'title', 'excerpt'),
-        'rewrite' => array('slug' => 'external','with_front' => false)  
-        )  
-    );  
+  // register wp_tool as a Custom Post Type
+  register_post_type('wp_tool',
+    array(  
+      'labels' => array(  
+        'name' => __( 'WordPress Tools' ),  
+        'singular_name' => __( 'WordPress Tool' )  
+      ),  
+      'public' => true,  
+      'menu_position' => 5,  
+      'supports' => array('title', 'editor', 'excerpt', 'thumbnail'),
+      'rewrite' => array('slug' => 'tool','with_front' => false)  
+    )  
+  );
 
-// connect external_post to category taxonomy
+  // connect wp_tool to category taxonomy
+  register_taxonomy_for_object_type('category', 'wp_tool');
 
-    register_taxonomy_for_object_type('category', 'external_post');
+  // register external_tool as a Custom Post Type
+  register_post_type('external_tool',
+    array(
+      'labels' => array(  
+          'name' => __( 'External Tools' ),  
+          'singular_name' => __( 'External Tool' )  
+      ),  
+      'public' => true,  
+      'menu_position' => 5,  
+      'supports' => array( 'title', 'excerpt', 'thumbnail'),
+      'rewrite' => array('slug' => 'special','with_front' => false)  
+    )  
+  );  
 
-// register wp_tool as a Custom Post Type
-
-    register_post_type( 'wp_tool',  
-        array(  
-            'labels' => array(  
-                'name' => __( 'WordPress Tools' ),  
-                'singular_name' => __( 'WordPress Tool' )  
-            ),  
-        'public' => true,  
-        'menu_position' => 5,  
-        'supports' => array( 'title', 'editor', 'excerpt', 'thumbnail'),
-        'rewrite' => array('slug' => 'tool','with_front' => false)  
-        )  
-    );  
-
-// connect wp_tool to category taxonomy
-
-    register_taxonomy_for_object_type('category', 'wp_tool');
-
-// register external_tool as a Custom Post Type
-
-    register_post_type( 'external_tool',  
-        array(  
-            'labels' => array(  
-                'name' => __( 'External Tools' ),  
-                'singular_name' => __( 'External Tool' )  
-            ),  
-        'public' => true,  
-        'menu_position' => 5,  
-        'supports' => array( 'title', 'excerpt', 'thumbnail'),
-        'rewrite' => array('slug' => 'special','with_front' => false)  
-        )  
-    );  
-
-// connect external_tool to category taxonomy
-
-    register_taxonomy_for_object_type('category', 'external_tool');
-
-
+  // connect external_tool to category taxonomy
+  register_taxonomy_for_object_type('category', 'external_tool');
 }  
-add_action( 'init', 'create_post_type' ); 
+
+add_action('init', 'create_post_type'); 
 
 // http://wordpress.stackexchange.com/questions/9211/changing-admin-menu-labels
-
 function change_post_menu_label() {
-    global $menu;
-    global $submenu;
-    $menu[5][0] = 'Original Reporting';
-    $submenu['edit.php'][5][0] = 'Original Reporting';
-    echo '';
+  global $menu;
+  global $submenu;
+  $menu[5][0] = 'Original Reporting';
+  $submenu['edit.php'][5][0] = 'Original Reporting';
+  echo '';
 }
 
-    add_action( 'admin_menu', 'change_post_menu_label' );
+add_action('admin_menu', 'change_post_menu_label');
 
-    add_post_type_support( 'external_post', 'source_meta' );
+add_post_type_support('external_post', 'source_meta');
 
 // Prevents HTML from being stripped for category description
 // http://wcdocs.woothemes.com/snippets/allow-html-in-term-category-tag-descriptions/
-
-foreach ( array( 'pre_term_description' ) as $filter ) {
-    remove_filter( $filter, 'wp_filter_kses' );
+foreach (array('pre_term_description') as $filter) {
+  remove_filter($filter, 'wp_filter_kses');
 }
  
-foreach ( array( 'term_description' ) as $filter ) {
-    remove_filter( $filter, 'wp_kses_data' );
+foreach (array('term_description') as $filter) {
+  remove_filter( $filter, 'wp_kses_data' );
 }
-
-
-
-
 
 function check_my_custom_post_thumbnail( $check, $post_id, $post_data ) {
   if ( $post_id <= 0 ) {
@@ -295,6 +275,7 @@ function check_my_custom_post_thumbnail( $check, $post_id, $post_data ) {
   }
 
   $categories = array(); //categories set by editor
+  
   if (is_array($post_data)) {
     $categories = $post_data['post_category'];
   } elseif (is_object($post_data)) {
@@ -314,9 +295,8 @@ function check_my_custom_post_thumbnail( $check, $post_id, $post_data ) {
     return true;
   }
 }
-add_filter( 'wypiekacz_check_thumbnail', 'check_my_custom_post_thumbnail', 10, 3);
 
-
-
+//TODO: What's the crazy wypiekacz mean?
+add_filter('wypiekacz_check_thumbnail', 'check_my_custom_post_thumbnail', 10, 3);
 
 ?>
