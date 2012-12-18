564d87d0834f050b618d35140d6c8cb00c7e7dfc
Casey Thomas <cpthomas@gmail.com>
2012-12-18 11:56:39 -0500

    Load real category description into compress background element

--- category.php
+++ category.php
@@ -28,8 +28,7 @@
               <span><?php echo get_category_by_slug($slug)->name ?></span>
             </div>
             <div class="content">
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-              <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+              <?php echo category_description( get_category_by_slug($slug)->term_id ); ?>             
             </div>
           </li>
         </ul>
