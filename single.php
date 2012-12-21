a8d235368ed3f05e541cc706b197ddc782363695
jfrankl <jfinbox@comcast.net>
2012-12-21 10:37:50 -0500

    move thumbnail caption function to functions.php

--- single.php
+++ single.php
@@ -76,18 +76,6 @@
               <div class="caption">
                 <p>
                   <?php
-                    //TODO: Can we move this to functions.php?
-                    function the_post_thumbnail_caption() {
-                      global $post;
-
-                      $thumbnail_id = get_post_thumbnail_id($post->ID);
-                      $thumbnail_image = get_posts(array('p' => $thumbnail_id, 'post_type' => 'attachment'));
-
-                      if ($thumbnail_image && isset($thumbnail_image[0])) {
-                        echo '<span>'.$thumbnail_image[0]->post_excerpt.'</span>';
-                      }
-                    }
-
                     the_post_thumbnail_caption();
                   ?>
                 </p>
