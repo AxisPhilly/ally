da7f77f56ac706d183861eee5c06b398da361ac9
jfrankl <jfinbox@comcast.net>
2013-01-03 12:25:07 -0500

    implement search. Closes #5

--- header.php
+++ header.php
@@ -11,8 +11,72 @@
   <script src="<?php bloginfo('template_directory'); ?>/javascripts/libraries.0.0.1.min.js" type="text/javascript"></script>
 <link rel="stylesheet" type="text/css" media="all" href="<?php bloginfo('template_directory'); ?>/stylesheets/app.css" />
 </head>
-<body <?php body_class(); ?>
+<body <?php body_class(); ?>>
   <!-- Fixed header -->
+
+  <div class="fixed contain-to-grid">
+    <nav class="top-bar">
+      <ul>
+        <li class="name">
+          <h1><a href="/demo/">AxisPhilly</a></h1>
+        </li>
+        <li class="toggle-topbar has-button">
+          <span class="tiny secondary button menu-button">
+            <span class="line"></span>
+            <span class="line"></span>
+            <span class="line"></span>
+          </span>
+        </li>
+      </ul>
+      <section>
+        <ul class="left">
+          <li class="divider"></li>
+          <li class="has-dropdown">
+            <a href="#">Our Projects</a>
+            <ul class="dropdown">
+              <li><a href="/projects/avi">AVI</a></li>
+              <li><a href="/projects/lobbying">Lobbying</a></li>
+              <li><a href="/demo/projects">More...</a></li>
+            </ul>
+          </li>
+          <li class="divider"></li>
+          <li><a href="#">Tools &amp; Data</a></li>
+          <li class="divider"></li>
+          <li><a href="/about">About</a></li>
+          <li class="search">
+          <li class="divider"></li>
+          <li>
+            <span class="social mobile">
+              <a href="twitter"><i class="social-foundicon-twitter"></i></a>
+              <a href="facebook"><i class="social-foundicon-facebook"></i></a>
+              <a href="googleplus"><i class="social-foundicon-google-plus"></i></a>
+              <a href="rss"><i class="social-foundicon-rss"></i></a>
+            </span>
+            <ul class="social desktop">
+              <li><a href="twitter"><i class="social-foundicon-twitter"></i></a></li>
+              <li><a href="facebook"><i class="social-foundicon-facebook"></i></a></li>
+              <li><a href="googleplus"><i class="social-foundicon-google-plus"></i></a></li>
+              <li><a href="rss"><i class="social-foundicon-rss"></i></a></li>
+            </ul>
+          </li>
+        </ul>
+        <ul class="right">
+          <li class="search">
+            <form class="collapse" role="search" method="get" id="searchform" action="<?php echo site_url(); ?>">
+              <input type="search" placeholder="Search" name="s" id="s"></input>
+            </form>
+          </li>
+        </ul>
+      </section>
+    </nav>
+  </div>
+
+
+  <!-- This is a WordPress generated header that doesn't quite work yet. It uses a custom Walker class, which is referred 
+
+
+
+
   <div class="fixed contain-to-grid">
     <nav class="top-bar">
       <ul>
@@ -46,4 +110,6 @@
         </ul>
       </section>
     </nav>
-  </div><!-- End fixed header -->
+  </div>
+
+End fixed header -->
---
+++ post-search.php
@@ -0,0 +1,27 @@
+<?php while (have_posts()) : the_post(); ?>
+       <article class="row">
+                  <?php if (has_post_thumbnail($post->ID)) { ?>  
+                    <div class="eight mobile-two columns">
+                  <? } else { ?>
+                    <div class="twelve mobile-four columns">
+                  <? } ?>
+                      <h4><a name="<?php the_id(); ?>" href="<?php if ((get_post_type() != 'external_post' )) the_permalink(); else echo get_post_meta( $post->ID, '_url_name', true); ?>"><?php the_title(); ?></a></h4>
+                      <div class="byline hide-for-small">
+                      <!-- Check to see if this is an external_post. If so, display Source and Source URL instead of Author. -->
+                        <?php
+                          if (get_post_type() == 'external_post') echo "Source: " . get_post_meta($post->ID, '_source_name', true); if ((get_post_type() != 'external_post')) { echo "by "; coauthors_posts_links(); } 
+                        ?>
+                      </div>
+                      <?php if ((get_post_type() != 'external_post')) { ?> 
+                        <div class="datetime hide-for-small"><?php the_time('F j, Y');?></div>
+                      <?php } ?>
+                      <div class="hide-for-small"><?php the_excerpt(); ?></div>
+                    </div>
+                  <?php if (has_post_thumbnail($post->ID)): ?>  
+                    <div class="four mobile-two columns">
+                      <?php $image = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' ); ?>
+                      <img src="<?php echo $image[0]; ?>">
+                    </div>
+                  <?php endif; ?>
+              </article>
+<?php endwhile; ?>
