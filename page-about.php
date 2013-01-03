9feb70f930206e91442d27ebc60f1779958b7d59
jfrankl <jfinbox@comcast.net>
2013-01-03 11:17:54 -0500

    create about template. Closes #18

---
+++ page-about.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * The template for displaying all pages.
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Eleven
+ * @since Twenty Eleven 1.0
+ */
+
+get_header(); ?>
+
+
+
+
+
+
+
+
+  <!-- Content -->
+  <div class="content-container about">
+    <div class="row">
+      <div class="two mobile-one columns">
+        <ul class="side-nav" id="about-menu" data-spy="affix" data-offset-top="5">
+          <li><a href="#mission">Mission</a></li>
+          <li><a href="#board">Board</a></li>
+          <li><a href="#staff">Staff</a></li>
+          <li><a href="#jobs">Jobs</a></li>
+          <li><a href="#inthenews">In The News</a></li>
+          <li><a href="#press">Press Releases</a></li>
+        </ul>
+      </div>
+
+      <div class="ten mobile-three columns">
+        <?php while ( have_posts() ) : the_post(); ?>
+
+          <?php the_content(); ?>
+
+        <?php endwhile; // end of the loop. ?>
+      </div>
+    </div>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+   
+<?php get_footer(); ?>
