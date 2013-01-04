30fd911a52e021610f88ad5be033a4e3707e62bc
Casey Thomas <cpthomas@gmail.com>
2013-01-04 15:52:00 -0500

    Change columns

--- footer.php
+++ footer.php
@@ -1,12 +1,12 @@
   <footer>
     <div class="row">
-      <div class="two columns">
+      <div class="three columns">
         <h3 class="subheader">AxisPhilly</h3>
         <p>
           © 2012 AxisPhilly
         </p>
       </div>
-      <div class="ten columns">
+      <div class="nine columns">
         <ul>
           <li><a href="/about">About Us</a></li>
           <li><a href="/about#mission">Mission</a></li>
