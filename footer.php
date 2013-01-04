eda434b1865bb49f63c5cdd53bf708615e47135e
Casey Thomas <cpthomas@gmail.com>
2013-01-04 15:48:50 -0500

    Add some content and styling to the footer

--- footer.php
+++ footer.php
@@ -1,3 +1,21 @@
-<footer></footer>
+  <footer>
+    <div class="row">
+      <div class="two columns">
+        <h3 class="subheader">AxisPhilly</h3>
+        <p>
+          © 2012 AxisPhilly
+        </p>
+      </div>
+      <div class="ten columns">
+        <ul>
+          <li><a href="/about">About Us</a></li>
+          <li><a href="/about#mission">Mission</a></li>
+          <li><a href="/about#staff">Staff</a></li>
+          <li><a href="/about#board">Board</a></li>
+          <li><a href="/about#jobs">Jobs</a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
 </body>
-</html> 
+</html>
