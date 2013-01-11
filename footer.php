febecb547a30d559653c677b42b300d7134c96d4
Casey Thomas <cpthomas@gmail.com>
2013-01-11 13:10:30 -0500

    FastClick is now kicked off in site.js

--- footer.php
+++ footer.php
@@ -18,12 +18,6 @@
     </div>
   </footer>
   <script src="<?php bloginfo('template_directory'); ?>/javascripts/foundation.min.js" type="text/javascript"></script>
-  <script type="text/javascript">
-    $(document).ready(function() {
-      window.addEventListener('load', function() {
-        new FastClick(document.body);
-      }, false);
-    });
-  </script>
+  <script src="<?php bloginfo('template_directory'); ?>/javascripts/site.js" type="text/javascript"></script>
 </body>
 </html>
