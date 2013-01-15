914d2349e782baada58a5a9c41329f356be10f86
Casey Thomas <cpthomas@gmail.com>
2013-01-15 12:23:49 -0500

    Remove temporary header buffer code

--- javascripts/site.js
+++ javascripts/site.js
@@ -15,16 +15,15 @@ axis.Collections.Articles = Backbone.Collection.extend({
 
 axis.Views.Header = Backbone.View.extend({
   initialize: function() {
-    var $spacer = $('<div>', {
+    /*var $spacer = $('<div>', {
       'class': 'spacer'
     });
 
     $(window).scroll(function() {
       if ($('.top-bar').hasClass('affix')) {
-        console.log('hey');
         $('.banner').prepend($spacer);
       }
-    });
+    });*/
   }
 });
 
