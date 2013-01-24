f2e030dfdc7ec61a1cb91561f1fc9c70451c7fe9
Casey Thomas <cpthomas@gmail.com>
2013-01-24 15:09:50 -0500

    Add tools to homepage

--- index.php
+++ index.php
@@ -30,9 +30,7 @@
           <div class="column-padding">
             <?php if(stripos($_SERVER["REQUEST_URI"], 'project/') == FALSE) { ?>
               <div class="category-symbology">
-                <?php
-                  list_categories();
-                ?>
+                <?php list_categories(); ?>
               </div>
             <?php } ?>          
               <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
@@ -68,11 +66,9 @@
           <?php endif; ?>        
           <div class="column-padding">
             <div class="category-symbology">
-              <?php
-                list_categories();
-              ?>
+              <?php list_categories(); ?>
             </div>  
-            <h3><a href=""><?php the_title(); ?></a></h3>
+            <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
             <div class="byline">by <?php coauthors_posts_links(); ?>—<?php the_time('M. j'); ?></div>
             <?php the_excerpt(); ?>
           </div>
@@ -100,11 +96,9 @@
         <article>
           <div class="column-padding">
             <div class="category-symbology">
-              <?php
-                list_categories();
-              ?>
+              <?php list_categories(); ?>
             </div>
-            <h3><a href=""><?php the_title(); ?></a></h3>
+            <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
             <div class="byline">by <?php coauthors_posts_links(); ?>—<?php the_time('M. j'); ?></div>
           </div>
         </article>
@@ -117,6 +111,7 @@
     <div class="three columns">
       <h2>Tools &amp; Data</h2>
       <p>We're building interactive maps, graphics, and applications to illuminate stories.</p>
+      <p><a href="/tools">View all of our tools</a></p>
     </div>
     <?php 
       $tools_args = array(
@@ -132,17 +127,67 @@
         while ($tools->have_posts()):
           $tools->the_post();
     ?>
-    <article class="three columns">
+    <article class="three columns tool">
       <?php if (has_post_thumbnail($post->ID)):  
         $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'thumbnail'); ?>
         <img src="<?php echo $image[0]; ?>">
       <?php endif; ?> 
-      <h3><a href=""><?php the_title(); ?></a></h3>
+      <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
       <div class="byline">by <?php coauthors_posts_links(); ?>—<?php the_time('M. j'); ?></div>
       <div><?php the_excerpt(); ?></div>
     </article>
     <?php endwhile; endif; ?>
     <?php wp_reset_postdata(); ?>
+  </section>
+  <section id="projects" class="row">
+    <div class="three columns">
+      <h2>Projects</h2>
+      <p>Our focus areas and investigations. Find tools, discussion, and curated stories from other media organizations on each topic.</p>
+    </div>
+    <?php 
+      $project_id = get_category_by_slug('project')->term_id;
+
+      $projects_args = array(
+        'parent' => strval($project_id),
+        'number' => 3
+      );
+
+      $projects = get_categories($projects_args);
+
+      foreach($projects as $project) {
+    ?>
+      <article class="project-summary three columns">
+        <h4><?php echo $project->name ?></h4>
+        <?php 
+          $latest_args = array(
+            'posts_per_page' => 1,
+            'category_name' => $project->slug
+          );
+
+          $latest = new WP_Query($latest_args);
+
+          if($latest->have_posts()) {
+            while($latest->have_posts()):
+              $latest->the_post();
+        ?>
+
+          <?php if (has_post_thumbnail($post->ID)):  
+            $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'thumbnail'); ?>
+            <img src="<?php echo $image[0]; ?>">
+          <?php endif; ?>        
+ 
+          <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
+          <div class="byline">by <?php coauthors_posts_links(); ?> — <?php the_time('M. j'); ?></div>
+          <?php the_excerpt(); ?>
+
+        <?php endwhile; } ?>
+        <?php wp_reset_postdata(); ?>
+        <a href="/project/<?php echo $project->slug; ?>">Go to project page</a>
+      </article>
+    <?php } ?>
+    <article class="project-summary three columns">
+      <p>More projects coming soon</p>
+    </article>
   </section>
 
 <?php get_footer(); ?>
