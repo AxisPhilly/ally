611bb627e8242097322b15b328b488853653a866
Casey Thomas <cpthomas@gmail.com>
2013-01-24 15:53:10 -0500

    Fix indentation

--- index.php
+++ index.php
@@ -1,111 +1,109 @@
-<?php 
-  get_header(); 
-  // Get Slug from URL
+<?php get_header(); ?>
+  <section class="row" id="homepage-content">
+    <div class="six columns" id="homepage-featured">
+      <?php
+        $featured_args = array(
+          'orderby' => 'date', 
+          'order' => 'DESC',
+          'posts_per_page' => 1,
+          'meta_info' => 'featured-homepage',
+          'meta_key' => '_thumbnail_id',
+          'post_status' => 'publish',
+          'post_type' => array('post', 'external_tool', 'wp_tool')
+        );
 
-?>
-<section class="row" id="homepage-content">
-      <div class="six columns" id="homepage-featured">
-        <?php
-          $featured_args = array(
-            'orderby' => 'date', 
-            'order' => 'DESC',
-            'posts_per_page' => 1,
-            'meta_info' => 'featured-homepage',
-            'meta_key' => '_thumbnail_id',
-            'post_status' => 'publish',
-            'post_type' => array('post', 'external_tool', 'wp_tool')
-          );
+        $featured = new WP_Query($featured_args);
 
-          $featured = new WP_Query($featured_args);
-
-          if ($featured->have_posts()):
-            while ($featured->have_posts()):
-              $featured->the_post();
-        ?>
-        <article>
-          <?php if (has_post_thumbnail($post->ID)):  
-            $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); ?>
-            <img src="<?php echo $image[0]; ?>">
-          <?php endif; ?>
-          <div class="column-padding">
-            <?php if(stripos($_SERVER["REQUEST_URI"], 'project/') == FALSE) { ?>
-              <div class="category-symbology">
-                <?php list_categories(); ?>
-              </div>
-            <?php } ?>          
-              <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-              <div class="byline">by <?php coauthors_posts_links(); ?> — <?php the_time('M. j'); ?></div>
-
-
-              <?php the_excerpt(); ?>
-            <?php endwhile; endif; ?>
-            <?php wp_reset_postdata(); ?>
-          </div>
-        </article>
-      </div>
-      <div class="three columns">
-        <?php
-          $latest_args = array(
-            'orderby' => 'date', 
-            'order' => 'DESC',
-            'posts_per_page' => 2,
-            'post_status' => 'publish',
-            'post_type' => array('post')
-          );
-
-          $latest = new WP_Query($latest_args);
-
-          if ($latest->have_posts()):
-            while ($latest->have_posts()):
-              $latest->the_post();
-        ?>
-        <article>
-          <?php if (has_post_thumbnail($post->ID)):  
-            $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'thumbnail'); ?>
-            <img src="<?php echo $image[0]; ?>">
-          <?php endif; ?>        
-          <div class="column-padding">
-            <div class="category-symbology">
-              <?php list_categories(); ?>
-            </div>  
-            <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
-            <div class="byline">by <?php coauthors_posts_links(); ?>—<?php the_time('M. j'); ?></div>
-            <?php the_excerpt(); ?>
-          </div>
-        </article>
-
-        <?php endwhile; endif; ?>
-        <?php wp_reset_postdata(); ?>
-      </div>
-      <div class="three columns">
-        <?php
-          $latest_args = array(
-            'orderby' => 'date', 
-            'order' => 'DESC',
-            'posts_per_page' => 5,
-            'post_status' => 'publish',
-            'post_type' => array('post')
-          );
-
-          $latest = new WP_Query($latest_args);
-
-          if ($latest->have_posts()):
-            while ($latest->have_posts()):
-              $latest->the_post();
-        ?>
-        <article>
-          <div class="column-padding">
+        if ($featured->have_posts()):
+          while ($featured->have_posts()):
+            $featured->the_post();
+      ?>
+      <article>
+        <?php if (has_post_thumbnail($post->ID)):  
+          $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'full'); ?>
+          <img src="<?php echo $image[0]; ?>">
+        <?php endif; ?>
+        <div class="column-padding">
+          <?php if(stripos($_SERVER["REQUEST_URI"], 'project/') == FALSE) { ?>
             <div class="category-symbology">
               <?php list_categories(); ?>
             </div>
+          <?php } ?>          
             <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-            <div class="byline">by <?php coauthors_posts_links(); ?>—<?php the_time('M. j'); ?></div>
-          </div>
-        </article>
+            <div class="byline">by <?php coauthors_posts_links(); ?> — <?php the_time('M. j'); ?></div>
 
-        <?php endwhile; endif; ?>
-        <?php wp_reset_postdata(); ?>
-      </div>
+
+            <?php the_excerpt(); ?>
+          <?php endwhile; endif; ?>
+          <?php wp_reset_postdata(); ?>
+        </div>
+      </article>
+    </div>
+    <div class="three columns">
+      <?php
+        $latest_args = array(
+          'orderby' => 'date', 
+          'order' => 'DESC',
+          'posts_per_page' => 2,
+          'post_status' => 'publish',
+          'post_type' => array('post')
+        );
+
+        $latest = new WP_Query($latest_args);
+
+        if ($latest->have_posts()):
+          while ($latest->have_posts()):
+            $latest->the_post();
+      ?>
+      <article>
+        <?php if (has_post_thumbnail($post->ID)):  
+          $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'thumbnail'); ?>
+          <img src="<?php echo $image[0]; ?>">
+        <?php endif; ?>        
+        <div class="column-padding">
+          <div class="category-symbology">
+            <?php list_categories(); ?>
+          </div>  
+          <h4><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h4>
+          <div class="byline">by <?php coauthors_posts_links(); ?>—<?php the_time('M. j'); ?></div>
+          <?php the_excerpt(); ?>
+        </div>
+      </article>
+
+      <?php endwhile; endif; ?>
+      <?php wp_reset_postdata(); ?>
+    </div>
+    <div class="three columns">
+      <?php
+        $latest_args = array(
+          'orderby' => 'date', 
+          'order' => 'DESC',
+          'posts_per_page' => 5,
+          'post_status' => 'publish',
+          'post_type' => array('post')
+        );
+
+        $latest = new WP_Query($latest_args);
+
+        if ($latest->have_posts()):
+          while ($latest->have_posts()):
+            $latest->the_post();
+      ?>
+      <article>
+        <div class="column-padding">
+          <div class="category-symbology">
+            <?php list_categories(); ?>
+          </div>
+          <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
+          <div class="byline">by <?php coauthors_posts_links(); ?>—<?php the_time('M. j'); ?></div>
+        </div>
+      </article>
+
+      <?php endwhile; endif; ?>
+      <?php wp_reset_postdata(); ?>
+
+      <a href="/">Story Archive</a>
+    </div>
   </section>
   <section id="tools" class="row">
     <div class="three columns">
@@ -175,7 +173,7 @@
             $image = wp_get_attachment_image_src(get_post_thumbnail_id($post->ID), 'thumbnail'); ?>
             <img src="<?php echo $image[0]; ?>">
           <?php endif; ?>        
- 
+
           <h5><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
           <div class="byline">by <?php coauthors_posts_links(); ?> — <?php the_time('M. j'); ?></div>
           <?php the_excerpt(); ?>
@@ -189,5 +187,4 @@
       <p>More projects coming soon</p>
     </article>
   </section>
-
 <?php get_footer(); ?>
