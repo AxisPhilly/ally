2993ec9c98caa90718e20366eca698bb3f652a8c
Casey Thomas <cpthomas@gmail.com>
2013-01-27 13:51:32 -0500

    Use 'discussion' in sidebar when viewing discussion

--- sidebar.php
+++ sidebar.php
@@ -20,7 +20,15 @@
           $c_slug = $category[0]->slug; 
       ?>
         <p>
-          <strong>This <?php if(get_post_type($post) == 'wp_tool') { echo 'tool'; } else { echo 'article'; } ?> is part of our series on <a href="/project/<?php echo $c_slug; ?>"><?php echo $c_name; ?></a>.
+          <strong>This <?php 
+            if(get_post_type($post) == 'wp_tool') { 
+              echo 'tool'; 
+            } elseif(get_post_type($post) == 'discussion') {
+              echo 'discussion';
+            } else { 
+              echo 'article'; 
+            } 
+          ?> is part of our series on <a href="/project/<?php echo $c_slug; ?>"><?php echo $c_name; ?></a>.
           Read more from the series:</strong>
         </p>
       <?php } else { ?>
