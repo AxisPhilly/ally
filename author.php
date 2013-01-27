ebe43b350f6318dc583c77736a23a5e9b808b275
Casey Thomas <cpthomas@gmail.com>
2013-01-27 12:57:50 -0500

    Cleaner author pages

--- author.php
+++ author.php
@@ -11,34 +11,33 @@
   <div class="content-container">
     <div id="news-container">
       <div class="row author">
-        <div class="photo four columns">
-          <? echo get_avatar( get_the_author_meta('ID'), 200); ?>
+        <div class="photo two columns">
+          <? echo get_avatar(get_the_author_meta('ID'), 200); ?>
         </div>
-        <div class="bio five columns">
-          <h2><?php echo $author->display_name ?></h2>
+        <div class="bio seven columns">
+          <strong><?php echo $author->display_name ?></strong>
           <p>
             <?php echo $author->user_description ?>
           </p>
         </div>
         <div class="contact three columns">
-          <ul>
-            <li><a href="mailto:<?php echo $author->user_email ?>"><?php echo $author->user_email ?></a></li>
-            <li><a href="http://twitter.com/<? echo $author->twitter ?>"><?php echo $author->twitter ?></a></li>
-            <li><?php echo $author->phone ?></li>
+          <ul class="no-markers">
+            <li><i class="general-foundicon-mail"></i> <a href="mailto:<?php echo $author->user_email ?>" target="_blank"><?php echo $author->user_email ?></a></li>
+            <li><i class="social-foundicon-twitter"></i> <a href="http://twitter.com/<? echo $author->twitter ?>" target="_blank">@<?php echo $author->twitter ?></a></li>
+            <li><i class="general-foundicon-phone"></i> <span><?php echo $author->phone ?></span></li>
           </ul>
         </div>
       </div>
       <div id="stories">
         <div class="items">
           <?php
-
             $author_args = array(
-                        'orderby' => 'date', 
-                        'order' => 'DESC',
-                        'posts_per_page' => -1,
-                        'author' => $author_id,
-                        'post_status' => 'publish',
-                        'post_type' => array('post', 'external_tool', 'wp_tool')
+              'orderby' => 'date', 
+              'order' => 'DESC',
+              'posts_per_page' => -1,
+              'author' => $author_id,
+              'post_status' => 'publish',
+              'post_type' => array('post', 'external_tool', 'wp_tool')
             );
             $author = new WP_Query($author_args);
             if ($author->have_posts()):
@@ -51,5 +50,4 @@
       </div>
     </div><!-- End News Container -->
   </div><!-- End Content Container -->
-<!-- Loads footer.php -->
 <?php  get_footer(); ?>
