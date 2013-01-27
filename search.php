e06198ce3401786074dca21858644244270f1a4f
Casey Thomas <cpthomas@gmail.com>
2013-01-27 13:26:33 -0500

    Smaller header

--- search.php
+++ search.php
@@ -14,13 +14,13 @@
       <div id="stories" class="twelve columns">
         <div class="items">
           <?php if (have_posts()): ?>
-          <h1 class="page-title"><?php printf(__('Search Results for: %s', 'ally'), '<span>' . get_search_query() . '</span>' ); ?></h1>
+          <h3 class="page-title"><?php printf(__('Search Results for: %s', 'ally'), '<span>' . get_search_query() . '</span>' ); ?></h3>
             <?php while (have_posts()) : the_post(); ?>
               <?php get_template_part('archive' , 'feed'); ?>
             <?php endwhile; ?>
           <?php else : ?>
             <div id="post-0" class="post no-results not-found">
-              <h2 class="entry-title"><?php _e('Nothing Found', 'ally'); ?></h2>
+              <h3 class="entry-title"><?php _e('Nothing Found', 'ally'); ?></h3>
               <div class="entry-content">
                 <p><?php _e('Sorry, but nothing matched your search criteria. Please try again with some different keywords.', 'Ally'); ?></p>
                 <?php get_search_form(); ?>
