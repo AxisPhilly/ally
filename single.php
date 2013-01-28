f07211f36ebba5a6e570b0c7b9a60f2b6d70f8d6
Jeff <jfinbox@comcast.net>
2013-01-28 08:02:03 -0500

    replace arrows in article navigation with flatland worthy html arrows. Closes #52

--- single.php
+++ single.php
@@ -52,10 +52,10 @@
           <nav class="article-navigation row">
             <ul>
               <li class="previous six columns mobile-two">
-                <?php previous_post_link('%link', '<strong><i class="general-foundicon-left-arrow"></i> Previous</strong><span>%title</span>'); ?>
+                <?php previous_post_link('%link', '<strong>&#8592; Previous</strong><span>%title</span>'); ?>
               </li>
               <li class="next six columns mobile-two">
-                <?php next_post_link('%link', '<strong>Next <i class="general-foundicon-right-arrow"></i></strong><span>%title</span>'); ?>
+                <?php next_post_link('%link', '<strong>Next &#8594;</strong><span>%title</span>'); ?>
               </li>
             </ul>
           </nav>
