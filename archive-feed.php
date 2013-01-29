2d7bd8c418c46ff2c322cbb5f54f9209f992c6cf
Casey Thomas <cpthomas@gmail.com>
2013-01-29 11:48:28 -0500

    Show byline and date on project news feed for mobile. Close #55

--- archive-feed.php
+++ archive-feed.php
@@ -24,20 +24,19 @@
           ?>
         </a>
       </h4>
-      <div class="byline-author <?php if(get_post_type() != 'external_post') { echo 'hide-for-small'; } ?>">
         <?php
           # Check to see if this is an external_post. If so, display Source and Source URL instead of Author.
-          if (get_post_type() == 'external_post') {
-            echo "<i>Source: <strong>" . get_post_meta($post->ID, '_source_name', true) . '</strong>' . 
-                  " - " . get_the_time('F j, Y') . "</i>";
-          }
-          elseif (get_post_type() != 'external_post') {
-        ?>
-          <div class="byline"><span class="byline-author">by <?php coauthors_posts_links(); ?> </span><span class="byline-date"><?php the_time('M. j'); ?></span></div>
-        <?
-          }
-        ?>
-      </div>
+          if (get_post_type() == 'external_post') { ?>
+            <div class="byline-author">
+              Source: <strong> <?php echo get_post_meta($post->ID, '_source_name', true); ?></strong> 
+              <?php echo get_the_time('F j, Y'); ?>
+            </div>
+         <?php } elseif (get_post_type() != 'external_post') { ?>
+          <div class="byline">
+            <span class="byline-author">by <?php coauthors_posts_links(); ?> </span>
+            <span class="byline-date"><?php the_time('M. j'); ?></span>
+          </div>
+        <?php } ?>
       <?php if ((get_post_type() != 'external_post')) { ?> 
       <?php } ?>
       <div class="hide-for-small">
