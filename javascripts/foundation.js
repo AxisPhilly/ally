8a345033b8680bd8f0c1146acd420b9171678431
Casey Thomas <cpthomas@gmail.com>
2013-01-29 10:31:29 -0500

    Add accordian file

--- javascripts/foundation.js
+++ javascripts/foundation.js
@@ -4,7 +4,7 @@
  Description: Custom theme for AxisPhilly
  Author: Casey Thomas, Jeff Frankl
  Author URI: caseypthomas.org, jfrankl.org
- Version: 0.5.5
+ Version: 0.9.0
 */
 /*!
  * jQuery Cookie Plugin v1.3
@@ -788,6 +788,54 @@
 		settings: settings
 	};
 });
+;(function ($, window, undefined){
+  'use strict';
+
+  $.fn.foundationAccordion = function (options) {
+
+    // DRY up the logic used to determine if the event logic should execute.
+    var hasHover = function(accordion) {
+      return accordion.hasClass('hover') && !Modernizr.touch
+    };
+
+    $(document).on('mouseenter', '.accordion li', function () {
+        var p = $(this).parent();
+
+        if (hasHover(p)) {
+          var flyout = $(this).children('.content').first();
+
+          $('.content', p).not(flyout).hide().parent('li').removeClass('active');
+          flyout.show(0, function () {
+            flyout.parent('li').addClass('active');
+          });
+        }
+      }
+    );
+
+    $(document).on('click.fndtn', '.accordion li .title', function () {
+        var li = $(this).closest('li'),
+            p = li.parent();
+
+        if(!hasHover(p)) {
+          var flyout = li.children('.content').first();
+
+          if (li.hasClass('active')) {
+            p.find('li').removeClass('active').end().find('.content').hide();
+          } else {
+            $('.content', p).not(flyout).hide().parent('li').removeClass('active');
+            flyout.show(0, function () {
+              flyout.parent('li').addClass('active');
+            });
+          }
+        }
+      }
+     );
+
+  };
+
+})( jQuery, this );
+
+
 ;(function ($, window, undefined) {
   'use strict';
   
