65226c83683358d21b6465c36a265b32f37010ad
Casey Thomas <cpthomas@gmail.com>
2013-01-30 12:11:51 -0500

    Update JS file

--- footer.php
+++ footer.php
@@ -21,6 +21,6 @@
     </div>
   </footer>
   <script src="<?php bloginfo('template_directory'); ?>/javascripts/foundation.min.js" type="text/javascript"></script>
-  <script src="<?php bloginfo('template_directory'); ?>/javascripts/site.0.9.0.min.js" type="text/javascript"></script>
+  <script src="<?php bloginfo('template_directory'); ?>/javascripts/site.1.0.0.min.js" type="text/javascript"></script>
 </body>
 </html>
