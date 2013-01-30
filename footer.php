2f53e325359d9807deb50c87f766ab06c4dcf4aa
jfrankl <jfinbox@comcast.net>
2013-01-30 18:49:49 -0500

    add subscription to footer

--- footer.php
+++ footer.php
@@ -10,7 +10,7 @@
         <img class="logo" alt="AxisPhilly" src="<?php bloginfo('template_directory'); ?>/images/logo-no-tagline-white.png"/>
         <p class="copyright">© 2013</p>
       </div>
-      <div class="nine columns">
+      <div class="three columns">
         <ul>
           <li><a href="/about">About Us</a></li>
           <li><a href="/about/contact-us">Contact Us</a></li>
@@ -18,6 +18,21 @@
           <li><a href="/about/user-agreement/">User Agreement</a></li>
         </ul>
       </div>
+      <section id="mc_embed_signup">
+        <div class="three columns">
+          <!-- Begin MailChimp Signup Form -->
+            <form action="http://neilbudde.us4.list-manage.com/subscribe/post?u=f2095380fddf560272aa750da&amp;id=fd22697bf6" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+              <label for="mce-EMAIL" id="email-signup-label">Sign up for email updates:</label>
+              <input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="email address" required>
+              <div class="clear"><input type="submit" value="Subscribe" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+            </form>
+
+          <!--End mc_embed_signup-->
+        </div>
+        <div class="three columns">
+        </div>
+      </section>        
+
     </div>
   </footer>
   <script src="<?php bloginfo('template_directory'); ?>/javascripts/foundation.min.js" type="text/javascript"></script>
